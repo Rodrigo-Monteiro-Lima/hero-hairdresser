4b34d7c4a170ce9d3bfccc96fb9afdb1f300694a
Rodrigo <rodrigoml2395@gmail.com>
2023-06-10 10:09:15 -0300

    feat: create user

--- api/src/services/UsersServices.ts
+++ api/src/services/UsersServices.ts
@@ -1,5 +1,18 @@
+import { ICreate } from "../interfaces/UsersInterface";
+import { UsersRepository } from "../repositories/UsersRepository";
+
 class UsersService {
-  create(user: { name: string; email: string; password: string; }) {
+  private userRepository: UsersRepository;
+  constructor() {
+    this.userRepository = new UsersRepository();
+  }
+  async create({name, email, password}: ICreate) {
+    const findUser = await this.userRepository.findUserByEmail(email);
+    if (findUser) {
+      throw new Error('User already exists');
+    }
+    const result = await this.userRepository.create({name, email, password});
+    return result;
   }
 }
 
