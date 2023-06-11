01254cb60cdda8cd6013537b1cedd0bdb53b6367
Rodrigo <rodrigoml2395@gmail.com>
2023-06-11 06:44:06 -0300

    feat: schedules routes

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application } from 'express'
 import { UsersRoutes } from './routes/users.routes'
 import { errorMiddleware } from './middlewares/error'
+import { SchedulesRoutes } from './routes/schedules.routes'
 
 const app:Application = express()
 
@@ -8,8 +9,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 const usersRoutes = new UsersRoutes().getRoutes()
+const schedulesRoutes = new SchedulesRoutes().getRoutes()
 
-app.use('/users', usersRoutes)
+app.use('/users', usersRoutes);
+app.use('/schedules', schedulesRoutes);
 
 app.use(errorMiddleware)
 
---
+++ api/src/routes/schedules.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+
+class SchedulesRoutes {
+  private router: Router
+  constructor() {
+    this.router = Router()
+  }
+  getRoutes(): Router {
+    this.router.post("/");
+    return this.router
+  }
+}
+
+export { SchedulesRoutes }
